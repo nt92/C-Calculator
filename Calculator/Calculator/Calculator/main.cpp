77b36fb3fd098745ed697fc4cf88a7df5bc05173
Nikhil Thota <nt92@ufl.edu>
2016-02-28 13:09:46 -0500

    added base project

---
+++ Calculator/Calculator/Calculator/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Calculator
+//
+//  Created by Nikhil Thota on 2/28/16.
+//  Copyright © 2016 Nikhil Thota. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "HEY!" << endl;
+    return 0;
+}
